550679bb50d527e3e8f41a7340da62480e0f7b43
Helen Pickavance <ellohez@gmail.com>
2024-01-29 11:36:36 +0000

    fix: further refactor of add function

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,12 +1,10 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b, ...rest) {
+export function add(...nums) {
   // Add a loop here
-  let result = a + b;
-  for(const num of rest) {
-    result += num;
-  }
+  let result = 0;
+  nums.forEach(num => result += num);
 
   return result;
 }
