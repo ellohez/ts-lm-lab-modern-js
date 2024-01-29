bcaa6b1f046eae33d2de3c6b7bf5e1f7ca057b8e
Helen Pickavance <ellohez@gmail.com>
2024-01-29 11:04:10 +0000

    fix: refactored introduction to use destructuring

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,14 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(a, b, ...rest) {
   // Add a loop here
-  return a + b;
+  let result = a + b;
+  for(const num of rest) {
+    result += num;
+  }
+
+  return result;
 }
 
 add(1, 2, 3, 4, 5);
